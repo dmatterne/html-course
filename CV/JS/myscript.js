d607226ed9a845f14c2e6ab8f79753a1ca677944
David Matterne <david.matterne@gmail.com>
2016-08-12 14:29:40 +0200

    added new files

---
+++ CV/JS/myscript.js
@@ -0,0 +1,10 @@
+       function showDate(){
+            var d = new Date(); // var , not date like in java
+            var age = prompt("Geef uw leeftijd op");
+            age = Number(age);
+            document.writeln(d);
+            document.writeln("Volgend jaar bent u " + (age + 1) + " jaar oud")
+
+            console.log("Testing 1 2 3");
+
+        }
